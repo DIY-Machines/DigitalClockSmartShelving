2d2b0daca2228f44f0e777795fd9cd2d8d8675c6
Ari Mandjelikian <arim215@users.noreply.github.com>
2020-10-20 23:03:12 -0400

    Update digits.ino

--- Shelf_Edge_Clock_V1/digits.ino
+++ Shelf_Edge_Clock_V1/digits.ino
@@ -1,46 +1,46 @@
-void digitZero(int offset, int colour){
+void digitZero(int offset, long int colour){
     stripClock.fill(colour, (0 + offset), 27);
     stripClock.fill(colour, (36 + offset), 27);
 }
 
-void digitOne(int offset, int colour){
+void digitOne(int offset, long int colour){
     stripClock.fill(colour, (0 + offset), 9);
     stripClock.fill(colour, (36 + offset), 9);
 }
 
-void digitTwo(int offset, int colour){
+void digitTwo(int offset, long int colour){
     stripClock.fill(colour, (0 + offset), 18);
     stripClock.fill(colour, (27 + offset), 9);
     stripClock.fill(colour, (45 + offset), 18);
 }
 
-void digitThree(int offset, int colour){
+void digitThree(int offset, long int colour){
     stripClock.fill(colour, (0 + offset), 18);
     stripClock.fill(colour, (27 + offset), 27);
 }
 
-void digitFour(int offset, int colour){
+void digitFour(int offset, long int colour){
     stripClock.fill(colour, (0 + offset), 9);
     stripClock.fill(colour, (18 + offset), 27);
 }
 
-void digitFive(int offset, int colour){
+void digitFive(int offset, long int colour){
     stripClock.fill(colour, (9 + offset), 45);
 }
 
-void digitSix(int offset, int colour){
+void digitSix(int offset, long int colour){
     stripClock.fill(colour, (9 + offset), 54);
 }
 
-void digitSeven(int offset, int colour){
+void digitSeven(int offset, long int colour){
     stripClock.fill(colour, (0 + offset), 18);
     stripClock.fill(colour, (36 + offset), 9);
 }
 
-void digitEight(int offset, int colour){
+void digitEight(int offset, long int colour){
     stripClock.fill(colour, (0 + offset), 63);
 }
 
-void digitNine(int offset, int colour){
+void digitNine(int offset, long int colour){
     stripClock.fill(colour, (0 + offset), 45);
 }
