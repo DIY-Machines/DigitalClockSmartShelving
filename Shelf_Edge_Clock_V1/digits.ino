00715ef9dce9586b5303b80a71cfce3e9bd2a041
Awdhoot Kanawade <awdhootkanawade@gmail.com>
2023-01-08 18:07:39 +0530

    Added leds per segment reference
    change the leds per segment ver in main sketch no further change required for custom sized clock

--- Shelf_Edge_Clock_V1/digits.ino
+++ Shelf_Edge_Clock_V1/digits.ino
@@ -1,46 +1,46 @@
 void digitZero(int offset, uint32_t colour){
-    stripClock.fill(colour, (0 + offset), 27);
-    stripClock.fill(colour, (36 + offset), 27);
+    stripClock.fill(colour, (0 + offset), (3*LED_PER_SEG));
+    stripClock.fill(colour, ((4*LED_PER_SEG) + offset), (3*LED_PER_SEG));
 }
 
 void digitOne(int offset, uint32_t colour){
-    stripClock.fill(colour, (0 + offset), 9);
-    stripClock.fill(colour, (36 + offset), 9);
+    stripClock.fill(colour, (0 + offset), LED_PER_SEG);
+    stripClock.fill(colour, ((4*LED_PER_SEG) + offset), LED_PER_SEG);
 }
 
 void digitTwo(int offset, uint32_t colour){
-    stripClock.fill(colour, (0 + offset), 18);
-    stripClock.fill(colour, (27 + offset), 9);
-    stripClock.fill(colour, (45 + offset), 18);
+    stripClock.fill(colour, (0 + offset), (2*LED_PER_SEG));
+    stripClock.fill(colour, ((3*LED_PER_SEG) + offset), LED_PER_SEG);
+    stripClock.fill(colour, ((5*LED_PER_SEG) + offset), (2*LED_PER_SEG));
 }
 
 void digitThree(int offset, uint32_t colour){
-    stripClock.fill(colour, (0 + offset), 18);
-    stripClock.fill(colour, (27 + offset), 27);
+    stripClock.fill(colour, (0 + offset), 14);
+    stripClock.fill(colour, ((3*LED_PER_SEG) + offset), (3*LED_PER_SEG));
 }
 
 void digitFour(int offset, uint32_t colour){
-    stripClock.fill(colour, (0 + offset), 9);
-    stripClock.fill(colour, (18 + offset), 27);
+    stripClock.fill(colour, (0 + offset), LED_PER_SEG);
+    stripClock.fill(colour, ((2*LED_PER_SEG) + offset), (3*LED_PER_SEG));
 }
 
 void digitFive(int offset, uint32_t colour){
-    stripClock.fill(colour, (9 + offset), 45);
+    stripClock.fill(colour, (LED_PER_SEG + offset), (5*LED_PER_SEG));
 }
 
 void digitSix(int offset, uint32_t colour){
-    stripClock.fill(colour, (9 + offset), 54);
+    stripClock.fill(colour, (LED_PER_SEG + offset), (6*LED_PER_SEG));
 }
 
 void digitSeven(int offset, uint32_t colour){
-    stripClock.fill(colour, (0 + offset), 18);
-    stripClock.fill(colour, (36 + offset), 9);
+    stripClock.fill(colour, (0 + offset), (2*LED_PER_SEG));
+    stripClock.fill(colour, ((4*LED_PER_SEG) + offset), LED_PER_SEG);
 }
 
 void digitEight(int offset, uint32_t colour){
-    stripClock.fill(colour, (0 + offset), 63);
+    stripClock.fill(colour, (0 + offset), (7*LED_PER_SEG));
 }
 
 void digitNine(int offset, uint32_t colour){
-    stripClock.fill(colour, (0 + offset), 45);
+    stripClock.fill(colour, (0 + offset), (6*LED_PER_SEG));
 }
